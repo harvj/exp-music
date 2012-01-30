6f0fc92ccd988e29e97ad596d17cdbc7e9a97732
Jim Harvey <jimrharvey@gmail.com>
2012-01-29 17:42:02 -0800

    fix syntax error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => "AWS",
-    :aws_access_key_id      => "AKIAIXJUMXD6CMQ6NPYA"
-    :aws_secret_access_key  => "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"
+    :aws_access_key_id      => "AKIAIXJUMXD6CMQ6NPYA",
+    :aws_secret_access_key  => "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",
     :region                 => "Oregon"
   }
   config.fog_directory = 'album-artwork'
