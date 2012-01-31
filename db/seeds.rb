345b3593da74a4b41440f4241a3727c425f02082
Jim Harvey <jimrharvey@gmail.com>
2012-01-30 23:04:11 -0800

    album seed data from itunes library

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+Artist.destroy_all
+Release.destroy_all
+Recording.destroy_all
+ReleaseTrack.destroy_all
+Genre.destroy_all
+
+CSV.foreach("#{Rails.root}/db/output.csv") do |row|
+  @given_name = row[0]
+  @given_artist = row[1]
+  @given_album = row[2]
+  @given_genre = row[3]
+  @given_length = row[4]
+  @given_no = row[5]
+
+  unless @genre = Genre.find_by_name(@given_genre)
+    @genre = Genre.create!(:name => @given_genre)
+  end
+
+  unless @artist = Artist.find_by_name(@given_artist)
+    @artist = Artist.create!(:name => @given_artist)
+  end
+
+  unless(@release = Release.find_by_name(@given_album) && @release.artists[0] == @artist)
+    @release = Release.create!(:name => @given_album)
+  else
+    @release = Release.find_by_name(@given_album)
+  end
+
+  @release.artists << @artist
+  @release.update_attributes(:genre_id => @genre.id)
+
+  @recording = Recording.create!(:name => @given_name, :length => @given_length.to_i)
+  @release.recordings << @recording
+
+  @tracks = ReleaseTrack.where(:recording_id => @recording.id, :release_id => @release.id)
+  @tracks[0].update_attributes(:track_no => @given_no.to_i)
+
+end
